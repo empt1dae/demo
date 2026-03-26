4cb5ae951b26f8820377b9f42fbd03b937454558
empt1dae <shortcutsnewyork@gmail.com>
2026-03-26 11:37:59 +0400

    Delete korochki directory

--- korochki/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = new mysqli("127.0.0.1", "root", "", "korochki");
-    if($conn->connect_error){
-        die("Ошибка  подключения!");
-    }
-
-    mysqli_set_charset($conn, "utf8mb4");
--- korochki/db_copy.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("10.0.52.8", "rwchqqqz", "0dZz6YuB6Kao", "rwchqqqz-m4");
-
-if ($conn->connect_error) {
-    die("Ошибка подключения!");
-}
-
-mysqli_set_charset($conn, "utf8mb4");
